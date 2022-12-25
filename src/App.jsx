6c2cfc91fb7c4edda53c8baf0678f9d89d83c011
Tamas Marki <tamas.marki@gmail.com>
2022-12-25 10:40:51 +0545

    fixed GA

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,5 @@
 import { ReactComponent as Logo } from './assets/bohuslind-logo-web.svg'
 import './App.scss';
-import ReactGA from 'react-ga';
-ReactGA.initialize('G-8NWYR3FE0B');
 
 function App() {
   return (
